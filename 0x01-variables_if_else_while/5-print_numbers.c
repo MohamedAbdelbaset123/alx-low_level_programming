15d2113c80e609b8e3a2475f9264d5a267d215bd
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-16 14:49:17 -0700

    C Programming Language 2

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a = 0;
+
+	while (a <= 9)
+	{
+		printf("%d", a);
+		a = a + 1;
+	}
+	printf("\n");
+	return (0);
+}
