bd00375c527f2c39db1c7b6b59e76b4a0b374bef
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-16 14:40:22 -0700

    C Programming Language 2

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != 'q' && a != 'e')
+		{
+			putchar(a);
+		}
+		a = a + 1;
+	}
+	putchar('\n');
+	return (0);
+}
