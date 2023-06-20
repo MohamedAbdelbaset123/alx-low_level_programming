f6eb6c8c1cf2d3705123872b616da4e2c79e8562
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-20 16:42:42 -0700

    C Programming Language 2

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - function
+ *
+ * @a: 1st input
+ *
+ * @b: 2nd input
+ *
+ * Return: `a + b`
+ */
+
+int add(int a, int b)
+{
+	int sum = a + b;
+
+	return (sum);
+}
