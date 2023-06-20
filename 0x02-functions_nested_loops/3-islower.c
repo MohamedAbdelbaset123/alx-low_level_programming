db87e581056988c665c72cfcde38788e350a17b9
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-20 11:26:16 -0700

    C Programming Language 2

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - Entry point
+ *
+ * @c: input of function
+ *
+ * Return: 1 if 'c' is lowercase
+ */
+
+int _islower(int c)
+{
+	int c = 'c'
+
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
