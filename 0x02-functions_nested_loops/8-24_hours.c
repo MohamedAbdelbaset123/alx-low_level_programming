8e098a09e44870105cd66ba773ecd0c362deee6d
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-20 16:00:59 -0700

    C Programming Language 2

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function
+ */
+
+void jack_bauer(void)
+{
+	int min = 0;
+	int hr = 0;
+
+	while (hr <= 23)
+	{
+		while (min <= 59)
+		{
+			_putchar((hr / 10) + 48);
+			_putchar((hr % 10) + 48);
+			_putchar(':');
+			_putchar((min / 10) + 48);
+			_putchar((min % 10) + 48);
+			_puchar('\n');
+			min = min + 1;
+		}
+		min = 0;
+		hr = hr + 1;
+	}
+}
