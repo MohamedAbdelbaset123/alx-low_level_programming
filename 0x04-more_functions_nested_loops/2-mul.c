626108b9723cae7c321f61fa463ef9ac02ec8b71
MohamedAbdelbaset123 <ma2001102001@gmail.com>
2023-06-22 15:22:49 -0700

    C Programming Language 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * mul - function
+ *
+ * @a: 1st input
+ *
+ * @b: 2nd input
+ *
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	int multi = a * b;
+
+	printf("%d", multi);
+	return (0);
+}
